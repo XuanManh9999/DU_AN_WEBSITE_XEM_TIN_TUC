228a702f4825d3be43587c1438a4eaa6b37bbde4
XuanManh9999 <nguyenxuanmanh2992003@gmail.com>
2025-11-18 08:20:46 +0000

    Current

--- src/main/java/com/datn/website_xem_tin_tuc/config/security/AppConfig.java
+++ src/main/java/com/datn/website_xem_tin_tuc/config/security/AppConfig.java
@@ -27,7 +27,7 @@ public class AppConfig {
     private final ManageUserService manageUserService;
     private final PreFilterSecurity preFilterSecurity;
     private final SecurityBeansConfig securityBeansConfig;
-    private String [] WHITE_LIST = {"/auth/**", "/api/v1/payment/**", "/api/v1/posts/**", "/api/v1/category-posts/**", "/api/v1/category-posts/by-category/**", "/api/v1/category/**", "/api/v1/articles/**"};
+    private String [] WHITE_LIST = {"/api/v1/auth/**", "/api/v1/payment/**", "/api/v1/posts/**", "/api/v1/category-posts/**", "/api/v1/category-posts/by-category/**", "/api/v1/category/**", "/api/v1/articles/**"};
 
     // config cors giup trinh duyet xac dinh cac origin nao duoc phep truy cap
     @Bean
@@ -36,7 +36,7 @@ public class AppConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://localhost:5174", "https://code-zen-education.vercel.app", "https://code-zen-admin-education.vercel.app", "https://codezen.io.vn")
+                        .allowedOrigins("https://admin-365-news.vercel.app", "https://365news.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(false)
--- src/main/java/com/datn/website_xem_tin_tuc/controller/AuthenticationController.java
+++ src/main/java/com/datn/website_xem_tin_tuc/controller/AuthenticationController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.thymeleaf.util.StringUtils;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/v1/auth")
 @Validated
 @Slf4j
 @RequiredArgsConstructor
